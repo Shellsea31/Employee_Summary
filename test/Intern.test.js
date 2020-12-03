280bd0e2efa27bedf8674fe3fe05dee4ad49ad3f
Shellsea <elainesbarrera@gmail.com>
2020-12-03 15:35:18 -0800

    TEST: getSchool function, passed

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -7,14 +7,14 @@ test("Can set school via constructor", () => {
   expect(e.school).toBe(testValue);
 });
 
-// 
-test("getRole() should return \"Intern\"", () => {
+// passed
+test('getRole() should return "Intern"', () => {
   const testValue = "Intern";
   const e = new Intern("Foo", 1, "test@test.com", "UCLA");
   expect(e.getRole()).toBe(testValue);
 });
 
-// 
+// passed
 test("Can get school via getSchool()", () => {
   const testValue = "UCLA";
   const e = new Intern("Foo", 1, "test@test.com", testValue);
