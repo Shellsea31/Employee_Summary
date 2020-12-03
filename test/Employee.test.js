461581ca68064e5037e3377e9443ccd22f9329d7
Shellsea <elainesbarrera@gmail.com>
2020-12-03 13:52:05 -0800

    TEST: set email, passed/ fixed id constructor value

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -20,7 +20,7 @@ test("Can set id via constructor argument", () => {
   expect(e.id).toBe(testValue);
 });
 
-// 
+// passed
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
