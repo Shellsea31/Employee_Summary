1d3a75a6d9f716edde220342f8100a4622580041
Shellsea <elainesbarrera@gmail.com>
2020-12-03 15:52:11 -0800

    TEST: getOfficeNumber function, passed, ALL TESTS PASS

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -15,7 +15,7 @@ test('getRole() should return "Manager"', () => {
   expect(e.getRole()).toBe(testValue);
 });
 
-// 
+// passed
 test("Can get office number via getOffice()", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
