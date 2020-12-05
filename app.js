6afe788a7af881dbdd2d0f47890ecb30ae429e2d
Shellsea <elainesbarrera@gmail.com>
2020-12-04 18:01:37 -0800

    UPDATE: export questions from question.js

--- app.js
+++ app.js
@@ -11,47 +11,10 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const { listenerCount } = require("process");
 const Employee = require("./lib/Employee");
+const questions = require("./lib/questions")
 
-const questions = [
-  {
-    type: "input",
-    name: "name",
-    message: "Type your name.",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "Type you id.",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Type you email.",
-  },
-  {
-    type: "list",
-    name: "role",
-    message: "What is your role?",
-    choices: [
-      {
-        name: "Employee",
-        value: "Employee",
-      },
-      {
-        name: "Engineer",
-        value: "Engineer",
-      },
-      {
-        name: "Intern",
-        value: "Intern",
-      },
-      {
-        name: "Manager",
-        value: "Manager",
-      },
-    ],
-  },
-];
+
+
 
 const getMembers = () => {
   inquirer
